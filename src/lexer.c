909a72ab54228f25d7bfdd53dd3da4d3a900c7ce
bampham <cbroo@duck.com>
2024-02-01 18:21:34 +0100

    tests, lexer
    added tests folder and tests for lexer.c. Extended next_token function in lexer.c

--- src/lexer.c
+++ src/lexer.c
@@ -3,12 +3,42 @@
 
 #include "lexer.h"
 
-Token next_token(Lexer lexer) {
+uint8_t peek(Lexer* lexer);
+void forward(Lexer* lexer);
+
+Token next_token(Lexer* lexer) {
 	Token token;
+
+	if (lexer->position >= strlen(lexer->input)) {
+		token.type = EOF_TYPE;
+		return token;
+	}
 	
+	// text token
+	if (lexer->ch == '\"') {
+		forward(lexer);
+		size_t i = 0;
+		while (lexer->ch != '\"') {
+			token.content[i] = lexer->ch;
+			forward(lexer);
+			++i;
+		}
+
+		token.type = TEXT;
+		forward(lexer);
+	}
+
+	// move one forward
+	forward(lexer);
+
 	return token;
 }
 
-uint8_t peek(Lexer lexer) {
-	return lexer.input[lexer.position + 1];
+uint8_t peek(Lexer* lexer) {
+	return lexer->input[lexer->position + 1];
+}
+
+void forward(Lexer* lexer) {
+	++lexer->position;
+	lexer->ch = lexer->input[lexer->position];
 }
---
+++ tests/lexer/test_lexer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../src/lexer.h"
+
+int main(int argc, char** argv) {
+
+	char* input = "test";
+	
+	// init lexer
+	Lexer lexer;
+	lexer.input = (char*)malloc(strlen(input) + 1);
+	strncpy(lexer.input, input, strlen(input));
+	lexer.input[strlen(input)] = '\0';
+
+	lexer.position = 0;
+	
+	lexer.ch = lexer.input[lexer.position];
+	// init lexer
+
+	Token token;
+	while ((token = next_token(&lexer)).type != EOF_TYPE) {
+		printf("%d\n", token.type);
+	}
+
+	free(lexer.input);
+	return 0;
+}
